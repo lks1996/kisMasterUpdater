5d4304c835a0f4b2602efb24a40dd6a80b3f1f21
lks1996 <mosplee@naver.com>
2026-01-19 19:33:46 +0900

    Kubernetes CronJob으로 배치 작업 전환

--- src/main/java/com/KisMasterUpdaterApplication.java
+++ src/main/java/com/KisMasterUpdaterApplication.java
@@ -1,13 +1,14 @@
 package com;
 
 import com.Service.KisMasterFileService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@Slf4j
 public class KisMasterUpdaterApplication implements CommandLineRunner  {
 
 	private final KisMasterFileService kisMasterFileService;
@@ -22,6 +23,13 @@ public class KisMasterUpdaterApplication implements CommandLineRunner  {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		kisMasterFileService.processAllMasterFiles();
+		log.warn("===== [START] Kis Master Updater =====");
+		try {
+			kisMasterFileService.processAllMasterFiles();
+			log.warn("===== [SUCCESS] Kis Master Updater =====");
+		} catch (Exception e) {
+			log.error("===== [FAIL] An error occurred during the scheduled job =====", e);
+		}
+		System.exit(0);
 	}
 }
